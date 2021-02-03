0849f53f0f18e9fd04f1bdcc9969b9704cb87aab
Jed Laundry <jlaundry@jlaundry.com>
2021-02-04 11:22:21 +1300

    deleteOriginal, log warning on ResourceNotFoundError

--- deleteOriginal/__init__.py
+++ deleteOriginal/__init__.py
@@ -2,6 +2,7 @@
 import logging
 import os
 
+from azure.core.exceptions import ResourceNotFoundError
 import azure.functions as func
 from azure.storage.blob import BlobServiceClient
 # from azure.storage.blob import RetentionPolicy
@@ -17,8 +18,11 @@ def main(msg: func.QueueMessage) -> None:
     # delete_retention_policy = RetentionPolicy(enabled=True, days=2)
     # blob_service_client.set_service_properties(delete_retention_policy=delete_retention_policy)
 
-    (container_name, blob_name) = infilename.split("/", 1)
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-    blob_client.delete_blob()
+    try:
+        (container_name, blob_name) = infilename.split("/", 1)
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+        blob_client.delete_blob()
+    except ResourceNotFoundError:
+        logging.warning(f"ResourceNotFoundError on {container_name}/{blob_name}")
 
     logging.info("Done.")
