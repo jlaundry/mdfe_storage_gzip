3a03a83e999cde91a9204138fa1025d61c9aab8d
Jed Laundry <jlaundry@jlaundry.com>
2021-09-02 11:17:01 +1200

    update Terraform with module

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 
+variable "azure_location" {
+  type = string
+  default = "australiaeast"
+}
+
 variable "env" {
-  type = map(string)
-  default = {
-    location = "Australia East"
-    environment = "Production"
-    shortcode = "aepr"
-  }
+  type = string
+  default = "prod"
 }
 
 variable "tags" {
@@ -19,17 +20,13 @@ variable "tags" {
 }
 
 resource "azurerm_resource_group" "mdfetelemetry" {
-  name     = "rg-${var.env.shortcode}-mdfe-telemetry"
-  location = var.env.location
+  name     = "rg-mdfe-telemetry-${var.env}-${var.azure_location}"
+  location = var.azure_location
 
   tags = var.tags
 }
 
 resource "random_id" "mdfetelemetry" {
-  byte_length = 4
-}
-
-resource "random_id" "mdfetelemetryfunc" {
   byte_length = 4
 }
 
@@ -52,80 +49,30 @@ resource "azurerm_storage_account" "mdfetelemetry" {
   tags = azurerm_resource_group.mdfetelemetry.tags
 }
 
+resource "azurerm_storage_container" "archive" {
+  name                  = "archive"
+  storage_account_name  = azurerm_storage_account.mdfetelemetry.name
+  container_access_type = "private"
+}
+
+resource "azurerm_management_lock" "mdfetelemetry-lock" {
+  name       = "nodelete"
+  scope      = azurerm_storage_account.mdfetelemetry.id
+  lock_level = "CanNotDelete"
+}
+
 output "mdfetelemetry" {
   value = azurerm_storage_account.mdfetelemetry.id
   description = "Data export location for MDfE configuration"
 }
 
-# Dedicated storage account for Function-related logs etc.
-resource "azurerm_storage_account" "mdfetelemetryfunc" {
-  name                     = "stmdfetelemfunc${random_id.mdfetelemetryfunc.hex}"
+module "mdfetelemetryfunc" {
+  source                   = "github.com/jlaundry/terraform-azure-library/func"
+
+  name                     = "mdfetelemetry"
+  env                      = var.env
   resource_group_name      = azurerm_resource_group.mdfetelemetry.name
   location                 = azurerm_resource_group.mdfetelemetry.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  min_tls_version = "TLS1_2"
-  allow_blob_public_access = false
-
-  tags = azurerm_resource_group.mdfetelemetry.tags
-}
-
-resource "azurerm_app_service_plan" "mdfetelemetryfunc" {
-  name                = "asp-${var.env.shortcode}-mdfe-telemetry-${random_id.mdfetelemetryfunc.hex}"
-  location            = azurerm_resource_group.mdfetelemetry.location
-  resource_group_name = azurerm_resource_group.mdfetelemetry.name
-  kind                = "FunctionApp"
-  reserved            = true
-
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
-
-  tags = azurerm_resource_group.mdfetelemetry.tags
-}
-
-resource "azurerm_application_insights" "mdfetelemetryfunc" {
-  name                = "appi-${var.env.shortcode}-mdfe-telemetry-${random_id.mdfetelemetryfunc.hex}"
-  location            = azurerm_resource_group.mdfetelemetry.location
-  resource_group_name = azurerm_resource_group.mdfetelemetry.name
-  application_type    = "web"
-
-  retention_in_days   = 90
-
-  tags = azurerm_resource_group.mdfetelemetry.tags
-}
-
-resource "azurerm_function_app" "mdfetelemetryfunc" {
-  name                       = "func-${var.env.shortcode}-mdfe-telemetry-${random_id.mdfetelemetryfunc.hex}"
-  location                   = azurerm_resource_group.mdfetelemetry.location
-  resource_group_name        = azurerm_resource_group.mdfetelemetry.name
-  app_service_plan_id        = azurerm_app_service_plan.mdfetelemetryfunc.id
-  storage_account_name       = azurerm_storage_account.mdfetelemetryfunc.name
-  storage_account_access_key = azurerm_storage_account.mdfetelemetryfunc.primary_access_key
-
-  version                    = "~3"
-  os_type                    = "linux"
-
-  app_settings = {
-    "AzureWebJobsStorage" = azurerm_storage_account.mdfetelemetryfunc.primary_blob_connection_string
-    "MDfETelemetryStorage" = azurerm_storage_account.mdfetelemetry.primary_blob_connection_string
-    # "WEBSITE_RUN_FROM_PACKAGE" = "1"
-    "FUNCTIONS_WORKER_RUNTIME" = "python"
-    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.mdfetelemetryfunc.instrumentation_key
-    "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.mdfetelemetryfunc.connection_string
-  }
-
-  site_config {
-    linux_fx_version  = "Python|3.8"
-    ftps_state        = "Disabled"
-  }
-
-  # source_control {
-  #   repo_url          = "https://github.com/jlaundry/mdfe_storage_gzip.git"
-  #   branch            = "main"
-  # }
 
   tags = azurerm_resource_group.mdfetelemetry.tags
 }
